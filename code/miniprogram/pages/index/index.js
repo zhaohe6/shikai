902bc588af118f9b7c2fbf8d7f6812ae3234e40a
gujiZhang <2621056214@qq.com>
2021-05-02 19:52:41 +0800

    第一次总更新

---
+++ code/miniprogram/pages/index/index.js
@@ -0,0 +1,86 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  },
+  formSubmit(e) {
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 1000,
+      mask:true
+  })
+    wx.cloud.callFunction({
+      name:"basicInfo",
+      data:{
+        info:e.detail.value
+      }
+    }).then(res=>{
+      if(res.errMsg=="cloud.callFunction:ok"){
+        wx.showToast({
+          title: '提交成功',
+          icon: 'succes',
+          duration: 1000,
+          mask:true
+      })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
